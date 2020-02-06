ce5640321ea3b7b4955e31ffec8aab418babfaa6
dxstanley <support@devexpress.com>
2020-02-06 18:49:31 +0300

    add CS content

---
+++ CS/DiagramBindToList/Models/Edge.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Web.ASPxDiagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiagramBindToList.Models
+{
+    public class Edge
+    {
+        public int ID { set; get; }
+        public string Text { set; get; }
+        public int? FromID { set; get; }
+        public int? ToID { set; get; }
+        //public ConnectorLineType LT { get; set; }
+
+        public Edge(int id, string text, int fromId, int toId)
+        {
+            ID = id;
+            Text = text;
+            FromID = fromId;
+            ToID = toId;
+        }
+
+        public Edge() { }
+    }
+}
---
+++ CS/DiagramBindToList/Models/Node.cs
@@ -0,0 +1,28 @@
+﻿using DevExpress.Web.ASPxDiagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiagramBindToList.Models
+{
+    public class Node
+    {
+        public int ID { set; get; }
+        public DiagramShapeType Type { set; get; }
+        public string Text { set; get; }
+        public int Width { set; get; }
+        public int Height { set; get; }
+
+        public Node(int id, DiagramShapeType type, string text, int width, int height)
+        {
+            ID = id;
+            Type = type;
+            Text = text;
+            Width = width;
+            Height = height;
+        }
+
+        public Node() {}
+    }
+}
